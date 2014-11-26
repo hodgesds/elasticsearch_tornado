55f490acbd7f0b742aed49c9da80f9af3e54bfc9
dan <hodges.daniel.scott@gmail.com>
2014-11-25 23:04:32 -0600

    updated test downloads of elasticsearch

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from __future__ import print_function
-
 import sys
-from os import environ
-from os.path import dirname, join, pardir, abspath, exists
+import tempfile
+from   os import environ, system
+from   os.path import dirname, join, pardir, abspath, exists
 import subprocess
 
 import nose
@@ -11,31 +11,39 @@ import nose
 
 def fetch_es_repo():
     # fetch new commits to be sure...
+    td = abspath(join(dirname(__file__), 'elasticsearch'))
+    system('mkdir -p %s' % td)
+    system('mkdir -p %s/tmp' % td)
     print('Fetching elasticsearch repo...')
-    subprocess.check_call('cd %s && git fetch https://github.com/elasticsearch/elasticsearch.git' % repo_path, shell=True)
-    # reset to the version fron info()
-    subprocess.check_call('cd %s && git reset --hard %s' % (repo_path, sha), shell=True)
+    #subprocess.check_call('cd %s && git clone https://github.com/elasticsearch/elasticsearch.git' % td, shell=True)
+    system('wget http://s3-eu-west-1.amazonaws.com/build-eu.elasticsearch.org/origin/master/nightly/JDK7/elasticsearch-latest-SNAPSHOT.tar.gz')
+    #subprocess.check_call('wget http://s3-eu-west-1.amazonaws.com/build-eu.elasticsearch.org/origin/master/nightly/JDK7/elasticsearch-latest-SNAPSHOT.tar.gz | tar xz --directory=%s/tmp --strip-components=1' % td)
+    print('aa')
+    subprocess.check_call('cd %s && git clone https://github.com/elasticsearch/elasticsearch.git' % td, shell=True)
+    subprocess.check_call('./elasticsearch')
+    return td
 
 
 def run_all(argv=None):
     sys.exitfunc = lambda: sys.stderr.write('Shutting down....\n')
 
     # fetch elasticsearch 
-    fetch_es_repo()
+    td = fetch_es_repo()
 
     # always insert coverage when running tests
     if argv is None:
         argv = [
             'nosetests', '--with-xunit',
-            '--with-xcoverage', '--cover-package=elasticsearch', '--cover-erase',
-            '--logging-filter=elasticsearch', '--logging-level=DEBUG',
-            '--verbose',
+            '--with-xcoverage', '--cover-package=elasticsearch_tornado',
+            '--cover-erase', '--logging-filter=elasticsearch',
+            '--logging-level=DEBUG', '--verbose',
         ]
 
     nose.run_exit(
         argv=argv,
         defaultTest=abspath(dirname(__file__))
     )
+    system('rm -rf %s' % td)
 
 if __name__ == '__main__':
     run_all(sys.argv)
