d39b5512d9c54453051dad00a5a1180f70ebfd94
dan <hodges.daniel.scott@gmail.com>
2014-12-03 23:27:53 -0600

    updated travis configs

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -10,28 +10,10 @@ import subprocess
 import nose
 
 
-def fetch_es_repo():
-    # fetch new commits to be sure...
-    td = abspath(join(dirname(__file__), 'test_elasticsearch'))
-    system('rm -rf %s' % td)
-    system('mkdir -p %s' % td)
-    #subprocess.check_call('cd %s && git clone https://github.com/elasticsearch/elasticsearch.git' % td, shell=True)
-    system('cd %s; wget http://s3-eu-west-1.amazonaws.com/build-eu.elasticsearch.org/origin/master/nightly/JDK7/elasticsearch-latest-SNAPSHOT.tar.gz; tar xvf elasticsearch*' % td)
-    #subprocess.check_call('cd %s; wget http://s3-eu-west-1.amazonaws.com/build-eu.elasticsearch.org/origin/master/nightly/JDK7/elasticsearch-latest-SNAPSHOT.tar.gz | tar xz --directory=%s/tmp --strip-components=1' % (td, td,))
-    #subprocess.check_call('cd %s && git clone https://github.com/elasticsearch/elasticsearch.git' % td, shell=True)
-    f = [x for x in popen("ls %s" % td).readlines() if '.gz' not in x][0]
-    el_dir = f.replace('\n','')
-    system('sh %s/%s/bin/elasticsearch &' % (td, el_dir,))
-    time.sleep(6)
-    return td
-
-
 def run_all(argv=None):
     sys.exitfunc = lambda: sys.stderr.write('Shutting down....\n')
 
     # fetch elasticsearch 
-    td = fetch_es_repo()
-    print('running tests')
     # always insert coverage when running tests
     if argv is None:
         argv = [
